c88f028ea745aecc9e7252ef1bff294f2a9a55ec
Daniel Afanador <danielthemisfits@gmail.com>
2022-06-30 17:47:59 -0300

    Programa para generar fechas con una función

---
+++ dates.py
@@ -0,0 +1,8 @@
+def date(year, month, day):
+    return month + " " + str(day) + ", " + str(year)
+
+print (date (2006, "July", 15))
+
+print (date (2004, "February", 4))
+
+print (date (1949, "June", 9))
